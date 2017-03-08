06002a3d98537dfffb9205599fdeaf27b1758166
Lucas Cross <lucascross@Lucass-iMac.local>
2017-03-08 17:54:26 -0500

    Added some content and a link from home to compare page and some visual aids so userws know how to use the site

--- src/shared/App/components/IndexPage.jsx
+++ src/shared/App/components/IndexPage.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import Link from 'react-router/lib/Link';
 
 const IndexPage = () => (
   <div className="">
     <div className="">
       <h1 className="o-heading-main">Welcome to The Civic Health Index</h1>
+      <div className="c-panel">
+        <div className="c-panel__head">
+          <h2 className="o-heading-sub">Tracking Civic Health</h2>
+        </div>
+        <div className="c-panel__content">
+          <p>
+            Check out our trend comparison system to compare two states against each other next to the national average.
+          </p>
+          <button className="c-ghost-button u-text-right">
+            <Link
+              className=""
+              to="/compare"
+            >
+              Compare States
+            </Link>
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 );
--- src/shared/Compare/components/Compare.jsx
+++ src/shared/Compare/components/Compare.jsx
@@ -1,10 +1,14 @@
 import React, { PropTypes } from 'react';
 
 import Trend from '../../Trend/components/Trend';
+import TrendSelect from '../../Select/components/TrendSelect';
 
 const Compare = props => (
   <div className="c-panel">
     <div>
+      <h1 className="o-heading-sub u-mb">Comparison System</h1>
+      <h2 className="u-mb-">Change the trend below to see more results</h2>
+      <TrendSelect {...props} />
       <Trend {...props} />
     </div>
   </div>
