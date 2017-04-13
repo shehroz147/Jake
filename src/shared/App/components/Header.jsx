3361311e76d565a740ae94503813fa8e8178719f
Lucas Cross <lucascross@Lucass-iMac.local>
2017-04-13 12:51:01 -0400

    updating styles

---
+++ src/shared/App/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => (
+
+  <div className="c-header c-header-box">
+    <div className="c-header-img" />
+    <div className="c-header-container">
+      <div className="">
+        <div className="">
+          <h1>Welcome to The Civic Health Index</h1>
+          <p>
+            A place for civic trend statistics. use the nav below to compare states or view or yearly reports.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+);
+
+export default Header;
--- src/shared/App/components/Layout.jsx
+++ src/shared/App/components/Layout.jsx
@@ -3,15 +3,17 @@ import React, { Children, PropTypes } from 'react';
 
 import Footer from './Footer';
 import Nav from './Nav';
+import Header from './Header';
 
 const Layout = ({ authenticated, children, logout }) => (
   <div className="o-wrapper">
     <div className="">
+      <Header />
       <Nav
         authenticated={authenticated}
         logout={logout}
       />
-      <div className="o-wrapper--main">
+      <div className="">
         {Children.only(children)}
       </div>
       <Footer />
