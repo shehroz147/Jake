2caf9404bd1edd6012af822171fbe046acae2f4d
Alucidisland <alucidisland@gmail.com>
2017-05-18 15:39:57 -0400

    moved imgs to different folders touched up sstyles

--- src/shared/Footer/components/PartnersFooter.jsx
+++ src/shared/Footer/components/PartnersFooter.jsx
@@ -2,19 +2,32 @@ import React from 'react';
 
 const PartnerFooter = () => (
   <div className="u-mb u-text-center u-pv">
-    <h1 className="o-heading-sub u-text-center">Partners in Civic Health</h1>
-    <a href="http://placehold.it" className="u-m">
-      <img src="http://placehold.it/150x150" alt="placeholder" />
-    </a>
-    <a href="http://placehold.it" className="u-m">
-      <img src="http://placehold.it/150x150" alt="placeholder" />
-    </a>
-    <a href="http://placehold.it" className="u-m">
-      <img src="http://placehold.it/150x150" alt="placeholder" />
-    </a>
-    <a href="http://placehold.it" className="u-m">
-      <img src="http://placehold.it/150x150" alt="placeholder" />
-    </a>
+    <h1 className="o-heading-sub u-text-center u-text--inverse">Partners in Civic Health</h1>
+    <div className="o-layout">
+      <div className="o-layout__item u-1/4">
+        <a href="" className="u-m">
+          <img src="/img/partners/lfiLogo.jpg" alt="Lou Frey Institute of Politicis & Government Logo" />
+        </a>
+      </div>
+
+      <div className="o-layout__item u-1/4">
+        <a href="" className="u-m">
+          <img src="/img/partners/fjcc.jpg" alt="The Florida Joint Center for Citizenship Logo" />
+        </a>
+      </div>
+
+      <div className="o-layout__item u-1/4">
+        <a href="" className="u-m">
+          <img src="/img/partners/bobgc_logo.png" alt="Bob Graham Center for Public Service Logo" />
+        </a>
+      </div>
+
+      <div className="o-layout__item u-1/4">
+        <a href="" className="u-m">
+          <img src="/img/partners/ncocLogo.jpg" alt="National Conference on Citizens Logo" />
+        </a>
+      </div>
+    </div>
   </div>
 );
 
--- src/shared/Reports/components/Reports.jsx
+++ src/shared/Reports/components/Reports.jsx
@@ -2,80 +2,92 @@ import React from 'react';
 
 const Reports = () => (
   <div className="o-wrapper">
+
     <div className="o-layout">
+
       <h1 className="o-layout__item o-heading-main">Civic Health Reports</h1>
-      <div className="c-panel o-layout__item u-1/2">
+
+      <div className="c-panel o-layout__item u-1/3">
+
         <div className="c-panel__head">
-          {/* <h1 className="c-panel__heading">2008 Report: "Beyond the Vote Cover"</h1> */}
+          <h1 className="c-panel__heading">2008 Report</h1>
         </div>
 
-        <div className="">
-          <a
-            href="https://drive.google.com/file/d/0B_H96_GL74SIWlFJZ2l5VEVsb0k/view?usp=sharing" rel="noopener noreferrer"
-            target="_blank"
-          >
-            <img
-              alt="Beyond the Vote Cover"
-              src="/img/health_civics_08.png"
-            />
-            <button className="c-button c-button--block ">
-              View
-            </button>
-          </a>
-        </div>
+        <a
+          href="https://drive.google.com/file/d/0B_H96_GL74SIWlFJZ2l5VEVsb0k/view?usp=sharing" rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img
+            alt="Beyond the Vote Cover"
+            src="/img/reports/health_civics_08.png"
+          />
+          <button className="c-button c-button--block ">
+            View
+          </button>
+        </a>
       </div>
 
-      <div className="c-panel o-layout__item u-1/2">
+      <div className="c-panel o-layout__item u-1/3">
+
         <div className="c-panel__head">
-          {/* <h1 className="c-panel__heading">2009 Report: "Communities and the State's Civic Destiny"</h1> */}
+          <h1 className="c-panel__heading">2009 Report</h1>
         </div>
+
         <a
           href="https://drive.google.com/file/d/0B_H96_GL74SId1M1dU05dm5yVUk/view?usp=sharing" rel="noopener noreferrer"
           target="_blank"
         >
           <img
             alt="Communities and the State's Civic Destiny"
-            src="/img/health_civics_09.png"
+            src="/img/reports/health_civics_09.png"
           />
           <button className="c-button c-button--block">
             View
           </button>
         </a>
+
       </div>
 
-      <div className="c-panel o-layout__item u-1/2">
+      <div className="c-panel o-layout__item u-1/3">
+
         <div className="c-panel__head">
-          {/* <h1 className="c-panel__heading">2010 Report: "A Tale of Two Citites"</h1> */}
+          <h1 className="c-panel__heading">2010 Report</h1>
         </div>
-          <a
-            href="https://drive.google.com/file/d/0B_H96_GL74SIT1R0U3Q2YnNzVzA/view?usp=sharing" rel="noopener noreferrer"
-            target="_blank"
-          >
-            <img
-              alt="A Tale of Two Citites"
-              src="/img/health_civics_10.png"
-            />
-            <button className="c-button c-button--block ">
-              View
-            </button>
-          </a>
+
+        <a
+          href="https://drive.google.com/file/d/0B_H96_GL74SIT1R0U3Q2YnNzVzA/view?usp=sharing" rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img
+            alt="A Tale of Two Citites"
+            src="/img/reports/health_civics_10.png"
+          />
+          <button className="c-button c-button--block ">
+            View
+          </button>
+        </a>
+
       </div>
-      <div className="c-panel o-layout__item u-1/2">
+
+      <div className="c-panel o-layout__item u-1/3">
+
         <div className="c-panel__head">
-          {/* <h1 className="c-panel__heading">2011 Report: "The Next Generation"</h1> */}
+          <h1 className="c-panel__heading">2011 Report</h1>
         </div>
+
         <a
           href="https://drive.google.com/file/d/0B_H96_GL74SIdjFiUUJ0TktKbm8/view?usp=sharing" rel="noopener noreferrer"
           target="_blank"
         >
           <img
             alt="The Next Generation"
-            src="/img/health_civics_11.png"
+            src="/img/reports/health_civics_11.png"
           />
           <button className="c-button c-button--block">
             View
           </button>
         </a>
+
       </div>
     </div>
   </div>
