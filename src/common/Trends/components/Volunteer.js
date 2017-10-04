1f43a248716faf6dc0aa5e5d62ae9214ba798162
Alucidisland <alucidisland@gmail.com>
2017-10-04 14:56:04 -0400

    Added donutgraph component to volunteer trend removed JSS from file added filter props

--- src/common/Trends/components/Volunteer.js
+++ src/common/Trends/components/Volunteer.js
@@ -1,84 +1,30 @@
 import React from 'react';
-import injectSheet from 'react-jss';
+
+import { DonutGraph } from '../../Graphs';
 
 const Volunteer = (props) => {
   const {
-    classes
+    filter: {
+      volunteer_percent,
+      volunteer_rank,
+    },
   } = props;
-  const percent = props.filter.volunteer_percent;
-  const math = 100 - props.filter.volunteer_percent;
-  const value = [
-    percent,
-    math,
-  ];
 
   return (
     <div>
       <h3>{props.state}</h3>
-      <div>
-        <svg
-          width="125px"
-          height="125px"
-          viewBox="0 0 42 42"
-          className="donut"
-          aria-labelledby=""
-          role="img"
-        >
-
-          <circle
-            className="donut-ring"
-            cx="21" cy="21" r="15.91549430918954"
-            fill="transparent"
-            stroke="#f7f7f7"
-            strokeWidth="3"
-            role="presentation"
-          />
-
-          <circle
-            className="donut-segment"
-            cx="21" cy="21" r="15.91549430918954"
-            fill="transparent"
-            stroke="#c0ffee"
-            strokeWidth="3"
-            strokeDasharray={value}
-            strokeDashoffset="25"
-          />
-
-          <g className={classes.text}>
-            <text x="50%" y="50%" className="chart-number">
-              {props.filter.volunteer_percent}%
-            </text>
-          </g>
-        </svg>
-      </div>
+      <DonutGraph percent={volunteer_percent}  />
 
       <ul>
-        <li>Percent: {props.filter.volunteer_percent}</li>
-        <li>Rank: {props.filter.volunteer_rank}</li>
-      </ul>
+        <li>Percent: {volunteer_percent}</li>
 
+        {volunteer_rank
+          ? <li>Rank: {volunteer_rank}</li>
+          : null
+        }
+      </ul>
     </div>
   );
-
-  if (props.filter.volunteer_rank === 0 || "") {
-    return (
-      <div>
-        <h3>{props.state}</h3>
-        <ul>
-          <li>Percent: {props.filter.volunteer_percent}</li>
-        </ul>
-      </div>
-    )
-  }
 }
 
-const styles = {
-  text: {
-    fontSize: "0.4em",
-    lineHeight: "1",
-    textAnchor: "middle",
-    transform: "translateY(-0.25em)",
-  }
-}
-
-export default injectSheet(styles)(Volunteer);
+export default Volunteer;
