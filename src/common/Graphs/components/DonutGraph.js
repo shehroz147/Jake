cbb26c74e93e67f6b7115855ee8876a1fee61209
Alucidisland <alucidisland@gmail.com>
2017-10-04 14:48:11 -0400

    Created donut graph component

--- src/common/Graphs/components/DonutGraph.js
+++ src/common/Graphs/components/DonutGraph.js
@@ -1,31 +1,64 @@
 import React from 'react';
 import injectSheet from 'react-jss';
 
-export const TREND_PERCENT = {
-  volunteer: 'volunteer_percent',
-  volunteer_hours: 'volunteer_hours_percent',
-  meeting: 'meeting_percent',
-  community: 'community_percent',
-  donated: 'donated_percent',
-  voted: 'vote_percent',
-  boycotted: 'product_percent',
-  registered: 'registered_percent',
-  group: 'group_percent',
-  organization: 'organization_percent',
-};
+const formatStrokeDashArray = (percent) => ([
+  percent,
+  (100 - percent),
+]);
 
 const DonutGraph = (props) => {
   const {
     classes,
+    percent,
   } = props;
 
-
-
   return (
+    <div>
+      <svg
+        width="125px"
+        height="125px"
+        viewBox="0 0 42 42"
+        className="donut"
+        aria-labelledby=""
+        role="img"
+      >
 
+        <circle
+          className="donut-ring"
+          cx="21" cy="21" r="15.91549430918954"
+          fill="transparent"
+          stroke="#f7f7f7"
+          strokeWidth="3"
+          role="presentation"
+        />
+
+        <circle
+          className="donut-segment"
+          cx="21" cy="21" r="15.91549430918954"
+          fill="transparent"
+          stroke="#c0ffee"
+          strokeWidth="3"
+          strokeDasharray={formatStrokeDashArray(percent)}
+          strokeDashoffset="25"
+        />
+
+        <g className={classes.text}>
+          <text x="50%" y="50%" className="chart-number">
+            {`${percent}%`}
+          </text>
+        </g>
+      </svg>
+    </div>
   );
+};
+
+const styles = {
+  text: {
+    fontSize: "0.4em",
+    lineHeight: "1",
+    textAnchor: "middle",
+    transform: "translateY(-0.25em)",
+  }
 }
-
-
 
 export default injectSheet(styles)(DonutGraph);
